849cb0d733aa575ac5885998f325295f065be51d
Ahmed <ahmedabdelmagied20@gmail.com>
2020-10-18 15:28:04 +0200

    refactor: integrate eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	tabWidth: 2,
+	arrowParens: 'always',
+	bracketSpacing: true,
+	printWidth: 100,
+};
